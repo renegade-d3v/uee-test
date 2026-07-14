7fcbcb7031e9f7b3d3e98fad6a6e8a08bb2aa055
Andrii S. <68372804+renegade-d3v@users.noreply.github.com>
2026-07-14 12:43:38 +0300

    Check for vendor directory and install dependencies
    Added a check to install composer dependencies if the vendor directory is missing.

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+# Install composer dependencies if vendor is missing
+if [ ! -f /var/www/vendor/autoload.php ]; then
+    composer setup
+fi
+
 # Create SQLite file if it doesn't exist
 if [ ! -f /var/www/database/database.sqlite ]; then
     touch /var/www/database/database.sqlite
